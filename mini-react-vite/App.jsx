acbbeb6005ec95a753ef67adc764c66b3b402672
gaozhengwei <gaozhengwei@chinasiwei.com>
2024-05-26 12:05:49 +0800

    vite实现jsx识别

---
+++ mini-react-vite/App.jsx
@@ -0,0 +1,17 @@
+import React from "./core/React.js";
+// const App = React.createElement(
+//   "div",
+//   {
+//     id: "app",
+//   },
+//   "hi",
+//   "react"
+// );
+// const App=<div id='app'>hi react</div> //JSX 会被转换成 React.createElement 调用。
+// console.log(App);
+// const Appone =()=>{
+//   return <div id='app'>hi react</div>
+// }
+// console.log(Appone);
+const App = <div id="app">hi react</div>;
+export default App;
